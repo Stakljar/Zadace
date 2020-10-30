9c4109926b5ff2505e90df86d0e81b5dda73fff8
Stakljar <73203225+Stakljar@users.noreply.github.com>
2020-10-30 22:59:10 +0100

    Add files via upload

---
+++ DZ1/DZ1 Solution/Class Library/Episode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Class_Library
+{
+    public class Episode
+    {
+        private int viewers;
+        private double totalScore;
+        private double maxScore;
+        public Episode()
+        {
+
+        }
+        public Episode(int viewers, double totalScore, double maxScore)
+        {
+            this.viewers = viewers;
+            this.totalScore = totalScore;
+            this.maxScore = maxScore;
+        }
+        public void AddView(double random)
+        {
+            viewers++;
+            totalScore += random;
+            if (maxScore < random) maxScore = random;
+
+        }
+        public double GetMaxScore()
+        {
+            return maxScore;
+        }
+        public double GetAverageScore()
+        {
+            return totalScore / viewers;
+        }
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+
+    }
+}

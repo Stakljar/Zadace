767e481d2e6aded3babb9721195aac1efccad8d4
Stakljar <73203225+Stakljar@users.noreply.github.com>
2020-11-06 00:16:12 +0100

    Add files via upload

---
+++ DZ2/DZ2 Solution/Class Library/Description.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Library
+{
+    public class Description
+    {
+        int number;
+        TimeSpan duration;
+        string name;
+        public Description() : this(0, new TimeSpan(0, 0, 0), "")
+        {
+        }
+        public Description(int number, TimeSpan duration, string name)
+        {
+            this.number = number;
+            this.duration = duration;
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return $"{number},{duration},{name}";
+        }
+
+    }
+}
---
+++ DZ2/DZ2 Solution/Class Library/TvUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Library
+{
+    public static class TvUtilities
+    {
+        static int count = 10;
+        public static double GenerateRandomScore()
+        {
+            Random random = new Random();
+            double randomNumber = (random.Next(100000, 1000000)) / 100000.0;
+            return randomNumber;
+        }
+        public static Episode Parse(string line)
+        {
+            string[] lines = line.Split(',');
+            int viewers = int.Parse(lines[0]);
+            double totalScore = double.Parse(lines[1]);
+            double maxScore = double.Parse(lines[2]);
+            int number = int.Parse(lines[3]);
+            TimeSpan duration = TimeSpan.Parse(lines[4]);
+            string name = lines[5];
+            return new Episode(viewers, totalScore, maxScore, new Description(number, duration, name));
+        }
+
+        public static void Sort(Episode[] episodes)
+        {
+            int sorted = 1;
+            Episode support = null;
+            while (sorted != 0)
+            {
+                sorted = 0;
+                for (int i = 0; i < episodes.Length - 1; i++)
+                {
+                    if (episodes[i] < episodes[i + 1])
+                    {
+                        sorted = 1;
+                        support = episodes[i];
+                        episodes[i] = episodes[i + 1];
+                        episodes[i + 1] = support;
+                    }
+
+                }
+
+            }
+        }
+    }
+}

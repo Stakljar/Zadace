570e87f81c903b0441ecabdb1474f978373df986
Stakljar <73203225+Stakljar@users.noreply.github.com>
2020-12-15 16:59:54 +0100

    Update Program.cs

--- DZ4/DZ4 Solution/DZ4/Program.cs
+++ DZ4/DZ4 Solution/DZ4/Program.cs
@@ -7,12 +7,7 @@ namespace DZ4
     class Program
     {
         static void Main(string[] args)
-        {   /*Umjesto pokazivača i dinamički alociranog polja u klasi koja predstavlja sezonu potrebno je rabiti List<Episode>. 
-            Omogućite iteriranje po sezoni korištenjem foreach petlje. Omogućite dodavanje novih epizoda na kraj sezone te uklanjanje epizoda iz sezone po imenu.
-                U slučaju da kod brisanja ne postoji niti jedna epizoda s navedenim imenom, baciti kao iznimku objekt vlastite klase izvedene iz klase Exception. 
-                Za sezonu definirajte odgovarajući konstruktor kopije tako da obavlja duboko kopiranje. 
-                Implementirajte sve potrebne klase i njihove metode kako bi se testni program u nastavku mogao ispravno izvesti.*/
-
+        {  
             // Assume that the number of rows in the text file is always at least 10. 
             // Assume a valid input file.
             string fileName = "shows.tv";

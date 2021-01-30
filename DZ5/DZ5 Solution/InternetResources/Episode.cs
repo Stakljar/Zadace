3f30783664dfd7e0dd4fadefff26f4f6eb621898
Stakljar <73203225+Stakljar@users.noreply.github.com>
2021-01-30 04:10:11 +0100

    Add files via upload

---
+++ DZ5/DZ5 Solution/InternetResources/Episode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetResources
+{
+    public class Episode
+    {
+        private string name;
+        private int number;
+        private string summary;
+        private int season;
+        public string Name { get => name; set => name = value; }
+        public int Number { get => number; set => number = value; }
+        public int Season { get => season; set => season = value; }
+        public string Summary { get => summary; set => summary = value; }
+
+        public override string ToString()
+        {
+            string overview = $"Episode {Number}. {Name}\n\n";
+            if (summary == null)
+                summary = "No preview accessible.";
+            for (int i = 0; i < Summary.Length; i++)
+            {
+                overview += Summary[i];
+                if (i % 75 == 0 && i != 0)
+                {
+                    if (summary[i] != ' ' && summary[i + 1] != ' ')
+                        overview += "-";
+                    overview += "\n";
+                }
+            }
+            overview += "\n";
+            return overview;
+        }
+
+    }
+}
---
+++ DZ5/DZ5 Solution/InternetResources/InternetServices.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InternetResources
+{
+    public static class InternetServices
+    {
+        public static Show ReadFromApi(string showTitle)
+        {
+            string netService = string.Empty;
+            Show show;
+            try
+            {
+                netService = new WebClient().DownloadString("http://api.tvmaze.com/singlesearch/shows?q=" + showTitle);
+                show = JsonConvert.DeserializeObject<Show>(netService);
+            }
+            catch (WebException e)
+            {
+                show = new Show();
+                show.Name = "Unknown";
+                show.Language = "Non existent";
+                show.Genres = new List<string> { "N/A" };
+                show.Summary = "No description available.";
+            }
+            for (int i = 0; i < show.Summary.Length - 3; i++)
+            {
+
+                if (show.Summary[i] == '<' && show.Summary[i + 1] == '/')
+                {
+                    show.Summary = show.Summary.Remove(i, 4);
+                    i--;
+                }
+                if (show.Summary[i] == '<' && show.Summary[i + 2] == '>')
+                {
+                    show.Summary = show.Summary.Remove(i, 3);
+                    i--;
+                }
+            }
+            try
+            {
+                netService = new WebClient().DownloadString("http://api.tvmaze.com/shows/" + $"{show.Id}" + "/seasons");
+                show.Seasons = JsonConvert.DeserializeObject<List<Season>>(netService);
+
+            }
+            catch (WebException e)
+            {
+                show.Seasons = new List<Season> { new Season() };
+                show.Seasons[0].Number = 0;
+            }
+            catch (JsonSerializationException e)
+            {
+                show.Seasons = new List<Season> { new Season() };
+                show.Seasons[0].Number = 0;
+
+
+
+            }
+            List<Episode> wholeShowEpisodes = new List<Episode>();
+            try
+            {
+                netService = new WebClient().DownloadString("http://api.tvmaze.com/shows/" + $"{show.Id}" + "/episodes");
+                wholeShowEpisodes = JsonConvert.DeserializeObject<List<Episode>>(netService);
+            }
+            catch (WebException e)
+            {
+                wholeShowEpisodes.Add(new Episode());
+                wholeShowEpisodes[0].Summary = "No preview available.";
+
+            }
+            for (int i = 0; i < wholeShowEpisodes.Count - 1; i++)
+            {
+                if (wholeShowEpisodes[i].Summary == null)
+                {
+                    wholeShowEpisodes[i].Summary = "";
+                }
+                for (int j = 0; j < wholeShowEpisodes[i].Summary.Length - 3; j++)
+                {
+
+                    if (wholeShowEpisodes[i].Summary[j] == '<' && wholeShowEpisodes[i].Summary[j + 1] == '/')
+                    {
+                        wholeShowEpisodes[i].Summary = wholeShowEpisodes[i].Summary.Remove(j, 4);
+                        j--;
+
+                    }
+                    if (wholeShowEpisodes[i].Summary[j] == '<' && wholeShowEpisodes[i].Summary[j + 2] == '>')
+                    {
+                        wholeShowEpisodes[i].Summary = wholeShowEpisodes[i].Summary.Remove(j, 3);
+                        j--;
+
+                    }
+                }
+            }
+
+            foreach (var season in show.Seasons)
+            {
+                List<Episode> seasonEpisodes = new List<Episode>();
+                foreach (var episode in wholeShowEpisodes)
+                {
+                    if (season.Number == episode.Season)
+                    {
+                        seasonEpisodes.Add(episode);
+                    }
+                }
+                season.Episodes = seasonEpisodes;
+            }
+            return show;
+        }
+    }
+}

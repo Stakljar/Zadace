2e19404fd5679267b5935971edc5552e9e317b0e
Stakljar <73203225+Stakljar@users.noreply.github.com>
2022-02-06 21:37:01 +0100

    Update Program.cs

--- DZ2/DZ2 Solution/DZ2/Program.cs
+++ DZ2/DZ2 Solution/DZ2/Program.cs
@@ -7,29 +7,29 @@ namespace DZ2
     class Program
     {
         static void Main(string[] args)
-        {
-			Description description = new Description(1, TimeSpan.FromMinutes(45), "Pilot");
-			Console.WriteLine(description);
-			Episode episode = new Episode(10, 88.64, 9.78, description);
-			Console.WriteLine(episode);
+	{
+		Description description = new Description(1, TimeSpan.FromMinutes(45), "Pilot");
+		Console.WriteLine(description);
+		Episode episode = new Episode(10, 88.64, 9.78, description);
+		Console.WriteLine(episode);
 
-			// Assume that the number of rows in the text file is always at least 10. 
-			// Assume a valid input file.
-			string fileName = "shows.tv";
-			string[] episodesInputs = File.ReadAllLines(fileName);
-			Episode[] episodes = new Episode[episodesInputs.Length];
-			for (int i = 0; i < episodes.Length; i++)
-			{
-				episodes[i] = TvUtilities.Parse(episodesInputs[i]);
-			}
-
-			Console.WriteLine("Episodes:");
-			Console.WriteLine(string.Join<Episode>(Environment.NewLine, episodes));
-			TvUtilities.Sort(episodes);
-			Console.WriteLine("Sorted episodes:");
-			string sortedEpisodesOutput = string.Join<Episode>(Environment.NewLine, episodes);
-			Console.WriteLine(sortedEpisodesOutput);
-			File.WriteAllText("sorted.tv", sortedEpisodesOutput);
+		// Assume that the number of rows in the text file is always at least 10. 
+		// Assume a valid input file.
+		string fileName = "shows.tv";
+		string[] episodesInputs = File.ReadAllLines(fileName);
+		Episode[] episodes = new Episode[episodesInputs.Length];
+		for (int i = 0; i < episodes.Length; i++)
+		{
+			episodes[i] = TvUtilities.Parse(episodesInputs[i]);
 		}
+
+		Console.WriteLine("Episodes:");
+		Console.WriteLine(string.Join<Episode>(Environment.NewLine, episodes));
+		TvUtilities.Sort(episodes);
+		Console.WriteLine("Sorted episodes:");
+		string sortedEpisodesOutput = string.Join<Episode>(Environment.NewLine, episodes);
+		Console.WriteLine(sortedEpisodesOutput);
+		File.WriteAllText("sorted.tv", sortedEpisodesOutput);
+	}
     }
 }

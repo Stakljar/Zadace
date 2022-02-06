f651936c9f54dd5df0b0bc0c5168f0ecbec26264
Stakljar <73203225+Stakljar@users.noreply.github.com>
2022-02-06 21:38:12 +0100

    Update Description.cs

--- DZ4/DZ4 Solution/Class Library/Description.cs
+++ DZ4/DZ4 Solution/Class Library/Description.cs
@@ -9,19 +9,21 @@ namespace Class_Library
         int lineUpNumber;
         TimeSpan duration;
         string name;
+        
         public Description() : this(0, new TimeSpan(0, 0, 0), "")
         {
         }
+        
         public Description(int lineUpNumber, TimeSpan duration, string name)
         {
             this.lineUpNumber = lineUpNumber;
             this.duration = duration;
             this.name = name;
         }
+        
         public override string ToString()
         {
             return $"{lineUpNumber},{duration},{name}";
         }
-
     }
 }
